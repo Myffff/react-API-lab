479f3c7a361322dc39e839c49756938b241b9072
Yifei Ma <634042441@qq.com>
2022-11-14 05:16:53 +0800

    added routing for movies and movie reviews.

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
