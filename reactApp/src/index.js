f47967d0a128fe58e8cbc8e288a717a609915b0b
Yifei Ma <634042441@qq.com>
2022-12-05 21:01:09 +0800

    Topic 12 - Movies context.

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -7,6 +7,7 @@ import SignUpPage from "./signUpPage";
 import AuthProvider from "./authContext";
 import AuthHeader from "./authHeader";
 import ProtectedRoutes from "./protectedRoutes";
+import MovieProvider from "./moviesContext";
 
 const App = () => {
   return (
@@ -27,19 +28,21 @@ const App = () => {
             <Link to="/profile">Profile</Link>
           </li>
         </ul>
-        <Routes>
-          <Route path="/public" element={<PublicPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
+        <MovieProvider>
+          <Routes>
+            <Route path="/public" element={<PublicPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
 
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
 
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </MovieProvider>
       </AuthProvider>
     </BrowserRouter>
   );
